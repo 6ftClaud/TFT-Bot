305a594c5d9985b695fd8ca90e9a33ebf476b494
Klaudijus Kungys <54559822+6ftClaud@users.noreply.github.com>
2021-08-23 21:35:31 +0300

    Delete PosScreen.py

--- PosScreen.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-
-print("Press Ctrl-C to quit")
-while True:
-    x, y = pyautogui.position()
-    positionStr = "X: " + str(x).rjust(4) + " Y: " + str(y).rjust(4)
-    print(positionStr, end="")
-    print("\b" * len(positionStr), end="", flush=True)
